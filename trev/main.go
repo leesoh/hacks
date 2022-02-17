f9b44284983774d9e6f32949102d753b6931778e
Liam Somerville <liam@liso.ca>
2022-02-17 10:34:51 -0700

    handle timezone

--- trev/main.go
+++ trev/main.go
@@ -9,22 +9,30 @@ import (
 )
 
 const (
-	iso_format = "2006-01-02 15:04:05"
+	iso_format = "2006-01-02 15:04:05 -0700"
 )
 
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	for sc.Scan() {
-		c := sc.Text()
-		// it's unix time
-		if len(c) == 10 {
-			utime, _ := strconv.ParseInt(c, 10, 64)
-			t := time.Unix(utime, 0)
-			fmt.Println(t.Format(iso_format))
-		} else {
-			itime, _ := time.Parse(iso_format, c)
-			fmt.Println(itime.Unix())
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			c := sc.Text()
+			printTime(c)
 		}
-
+	} else {
+		printTime(os.Args[1])
 	}
+}
+
+func printTime(tm string) {
+	if len(tm) == 10 {
+		utime, _ := strconv.ParseInt(tm, 10, 64)
+		t := time.Unix(utime, 0)
+		fmt.Println(t.Format(iso_format))
+	} else {
+		itime, _ := time.Parse(iso_format, tm)
+		fmt.Println(itime.Unix())
+	}
+
 }
